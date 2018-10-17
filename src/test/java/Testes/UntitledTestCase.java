7a242388ee5f0f5ac264e7e438e16a51b83bfc28
semana <semana@semana-da-computacao-2018>
2018-10-16 22:31:37 -0300

    teste

---
+++ src/test/java/Testes/UntitledTestCase.java
@@ -0,0 +1,79 @@
+package Testes;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class UntitledTestCase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.gecko.driver", "/home/semana/Downloads/geckodriver");
+      driver = new FirefoxDriver();
+    
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    driver.get("http://localhost:8080/MinicursoSelenium/");
+    driver.findElement(By.id("userLogin")).click();
+    driver.findElement(By.id("userLogin")).clear();
+    driver.findElement(By.id("userLogin")).sendKeys("participante2@teste.com");
+    driver.findElement(By.id("userPassword")).clear();
+    driver.findElement(By.id("userPassword")).sendKeys("1234");
+    driver.findElement(By.name("acessar")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
